e98688184ac2e74bddfc11edcb2047a7adfda642
Brenda Delmondes <brenda2004delmondes@gmail.com>
2022-11-11 19:14:46 -0300

    Add files via upload

---
+++ sistema/frm_categoria.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace sistema
+{
+    partial class frm_categoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_cancelar = new System.Windows.Forms.Button();
+            this.btn_excluir = new System.Windows.Forms.Button();
+            this.btn_cadastrar = new System.Windows.Forms.Button();
+            this.btn_novo = new System.Windows.Forms.Button();
+            this.CB_categoria = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_valor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_descriçao = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(56, 223);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(692, 190);
+            this.dataGridView1.TabIndex = 21;
+            // 
+            // btn_cancelar
+            // 
+            this.btn_cancelar.Location = new System.Drawing.Point(516, 130);
+            this.btn_cancelar.Name = "btn_cancelar";
+            this.btn_cancelar.Size = new System.Drawing.Size(75, 23);
+            this.btn_cancelar.TabIndex = 20;
+            this.btn_cancelar.Text = "Cancelar";
+            this.btn_cancelar.UseVisualStyleBackColor = true;
+            // 
+            // btn_excluir
+            // 
+            this.btn_excluir.Location = new System.Drawing.Point(392, 131);
+            this.btn_excluir.Name = "btn_excluir";
+            this.btn_excluir.Size = new System.Drawing.Size(75, 23);
+            this.btn_excluir.TabIndex = 19;
+            this.btn_excluir.Text = "Excluir";
+            this.btn_excluir.UseVisualStyleBackColor = true;
+            // 
+            // btn_cadastrar
+            // 
+            this.btn_cadastrar.Location = new System.Drawing.Point(265, 131);
+            this.btn_cadastrar.Name = "btn_cadastrar";
+            this.btn_cadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btn_cadastrar.TabIndex = 18;
+            this.btn_cadastrar.Text = "Cadastrar";
+            this.btn_cadastrar.UseVisualStyleBackColor = true;
+            // 
+            // btn_novo
+            // 
+            this.btn_novo.Location = new System.Drawing.Point(127, 131);
+            this.btn_novo.Name = "btn_novo";
+            this.btn_novo.Size = new System.Drawing.Size(75, 23);
+            this.btn_novo.TabIndex = 17;
+            this.btn_novo.Text = "Novo";
+            this.btn_novo.UseVisualStyleBackColor = true;
+            // 
+            // CB_categoria
+            // 
+            this.CB_categoria.FormattingEnabled = true;
+            this.CB_categoria.Location = new System.Drawing.Point(596, 38);
+            this.CB_categoria.Name = "CB_categoria";
+            this.CB_categoria.Size = new System.Drawing.Size(121, 21);
+            this.CB_categoria.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(539, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "categoria";
+            // 
+            // txt_valor
+            // 
+            this.txt_valor.Location = new System.Drawing.Point(346, 39);
+            this.txt_valor.Name = "txt_valor";
+            this.txt_valor.Size = new System.Drawing.Size(121, 20);
+            this.txt_valor.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(310, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "valor";
+            // 
+            // txt_descriçao
+            // 
+            this.txt_descriçao.Location = new System.Drawing.Point(127, 39);
+            this.txt_descriçao.Name = "txt_descriçao";
+            this.txt_descriçao.Size = new System.Drawing.Size(121, 20);
+            this.txt_descriçao.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Descrição";
+            // 
+            // frm_categoria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_cancelar);
+            this.Controls.Add(this.btn_excluir);
+            this.Controls.Add(this.btn_cadastrar);
+            this.Controls.Add(this.btn_novo);
+            this.Controls.Add(this.CB_categoria);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_valor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_descriçao);
+            this.Controls.Add(this.label1);
+            this.Name = "frm_categoria";
+            this.Text = "frm_categoria";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_cancelar;
+        private System.Windows.Forms.Button btn_excluir;
+        private System.Windows.Forms.Button btn_cadastrar;
+        private System.Windows.Forms.Button btn_novo;
+        private System.Windows.Forms.ComboBox CB_categoria;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_valor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_descriçao;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ sistema/frm_menu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sistema
+{
+    public partial class frm_menu : Form
+    {
+        public frm_menu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           frm_produtos frm = new frm_produtos();
+            frm.ShowDialog();
+        }
+
+        private void frm_menu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_cad_categorias_Click(object sender, EventArgs e)
+        {
+            frm_Categorias frm = new frm_Categorias();
+            frm.ShowDialog();
+        }
+
+        private void produtosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_produtos frm = new frm_produtos();
+            frm.ShowDialog();
+        }
+
+        private void categoriasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_Categorias frm = new frm_Categorias();
+            frm.ShowDialog();
+        }
+    }
+}
